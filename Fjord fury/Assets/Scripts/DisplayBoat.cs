fa53b5fd561e5b3044e875aad3e2485d0f3bd061
Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
2025-10-07 16:17:05 +0200

    displayboat script
    shouldnt jutter too much :)

---
+++ Fjord fury/Assets/Scripts/DisplayBoat.cs
@@ -0,0 +1,140 @@
+using Unity.Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+
+public class DisplayBoat: MonoBehaviour
+{
+    public CinemachineSplineCart dollykart;
+    public SplineTrack CurrentTrack;
+
+    public Collider ColRef;
+    bool grounded = true;
+    bool jumping;
+    float LRsteer;
+    public float SteerSpeed = 10, jumpPower = 10, gravity = 5, quickfallSpeed = 20, ForwardSpeed = 40;
+    float ySpeed, resetLerp;
+    bool deathLerp;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        setSpeed(ForwardSpeed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LerpPos();
+
+        transform.localPosition += Vector3.right * LRsteer * Time.deltaTime * SteerSpeed;
+
+
+        if (!grounded)
+        {
+            
+            transform.localPosition += Vector3.up * ySpeed * Time.deltaTime;
+            ySpeed -= Time.deltaTime * gravity;
+            if (!jumping) ySpeed -= Time.deltaTime * quickfallSpeed;
+
+            if (jumping && ySpeed < 0) jumping = false;
+
+            if (ySpeed < 0 && transform.localPosition.y < -10)
+            {
+                //missed the road, uh oh
+                print("oops you died");
+                deathLerp = true;
+                resetLerp = .5f;
+                jumping = false;
+                grounded = true;
+                ySpeed = 0;
+
+
+
+
+
+            }
+        }
+        else // grounded
+        {
+            if (Mathf.Abs(transform.localPosition.x) > CurrentTrack.width)
+            {
+                transform.localPosition = new Vector3(CurrentTrack.width * Mathf.Sign(transform.localPosition.x), transform.localPosition.y, transform.localPosition.z);
+            }
+        }
+
+    }
+    public void OnHorizontal(InputValue value)
+    {
+        float p = value.Get<float>();
+
+        //transform.localPosition += Vector3.right * p;
+        LRsteer = p;
+    }
+
+    public void OnJump(InputValue value)
+    {
+        print("JUMPS: " + value.Get());
+        if (grounded && value.Get() != null)
+        {
+            StartCoroutine(DisableColliderBriefly());
+            //jump()
+            
+            grounded = false;
+            ySpeed = jumpPower;
+            jumping = true;
+        }
+        else if (value.Get() == null) { jumping = false; }
+    }
+
+    public void OnH(InputValue inputValue)
+    {
+        dollykart.SplinePosition += 0.1f * 100000;
+        print("HYPEERJUMP");
+    }
+    void setSpeed(float newSpeed)
+    {
+        var autodolly = dollykart.AutomaticDolly.Method as SplineAutoDolly.FixedSpeed;
+        if (autodolly != null)
+        {
+
+            autodolly.Speed = newSpeed;
+        }
+
+    }
+    void LerpPos()
+    {
+        if (resetLerp > 0)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(deathLerp ? 0 : transform.localPosition.x, 0, 0), 1 - resetLerp);
+            resetLerp -= Time.deltaTime;
+        }
+        else
+        {
+            deathLerp = false;
+        }
+    }
+    IEnumerator DisableColliderBriefly()
+    {
+        ColRef.enabled = false;
+        yield return new WaitForSeconds(0.15f);
+        ColRef.enabled = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+
+        Debug.Log(other.name);
+        if (other.gameObject.TryGetComponent<SplineTrack>(out SplineTrack ST) && !grounded)
+        {
+            //landing
+            grounded = true;
+
+            
+            jumping = false;
+
+
+            ySpeed = 0;
+            
+        }
+    }
+}

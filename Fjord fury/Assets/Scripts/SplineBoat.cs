5a1b6e15bb029245c1f3c6a2f536c4ecf81be425
Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
2025-10-07 15:30:52 +0200

    UnSpline when jump

--- Fjord fury/Assets/Scripts/SplineBoat.cs
+++ Fjord fury/Assets/Scripts/SplineBoat.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,11 +8,13 @@ public class SplineBoat : MonoBehaviour
     public CinemachineSplineCart dollykart;
     public SplineTrack CurrentTrack;
 
+    public Collider ColRef;
     bool grounded = true;
     bool jumping;
     float LRsteer;
     public float SteerSpeed=10, jumpPower=10, gravity=5, quickfallSpeed=20, ForwardSpeed=40;
-    float ySpeed;
+    float ySpeed,resetLerp;
+    bool deathLerp;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,34 +24,35 @@ public class SplineBoat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        LerpPos();
+
         transform.localPosition += Vector3.right * LRsteer * Time.deltaTime * SteerSpeed;
 
      
         if (!grounded)
         {
+            transform.localPosition += Vector3.forward * ForwardSpeed * Time.deltaTime;
             transform.localPosition += Vector3.up * ySpeed*Time.deltaTime;
             ySpeed -= Time.deltaTime*gravity;
             if(!jumping) ySpeed -= Time.deltaTime*quickfallSpeed;
             
             if(jumping && ySpeed < 0) jumping = false;
 
-            if (ySpeed < 0 && transform.localPosition.y < 0)
+            if (ySpeed < 0 && transform.localPosition.y < -10)
             {
-                if(Mathf.Abs(transform.localPosition.x) > CurrentTrack.width)
-                {
-                    //missed the road, uh oh
-                    print("oops you died");
-                    transform.localPosition = Vector3.zero;
-                    jumping = false;
-                    grounded = true;
-                }
-                else
-                {
-                    //landing
-                    jumping = false;
-                    grounded = true;
-                    transform.localPosition = new Vector3(transform.localPosition.x,0,transform.localPosition.z);
-                }
+                //missed the road, uh oh
+                print("oops you died");
+                deathLerp = true;
+                resetLerp = 0.4f;
+                jumping = false;
+                dollykart.AutomaticDolly.Enabled = true;
+                grounded = true;
+                ySpeed = 0;
+                
+
+                
+                
+                    
             }
         }
         else // grounded
@@ -73,7 +77,9 @@ public class SplineBoat : MonoBehaviour
         print("JUMPS: "+ value.Get());
         if(grounded && value.Get()!= null) 
         {
+            StartCoroutine(DisableColliderBriefly());
             //jump()
+            dollykart.AutomaticDolly.Enabled = false;
             grounded = false;
             ySpeed = jumpPower;
             jumping = true;
@@ -81,13 +87,54 @@ public class SplineBoat : MonoBehaviour
         else if(value.Get()== null) { jumping = false; }
     }
 
+    public void OnH(InputValue inputValue)
+    {
+        dollykart.SplinePosition += 0.1f*100000;
+        print("HYPEERJUMP");   
+    }
     void setSpeed(float newSpeed)
     {
         var autodolly = dollykart.AutomaticDolly.Method as SplineAutoDolly.FixedSpeed;
         if (autodolly != null)
         {
+
             autodolly.Speed = newSpeed;
         }
 
+    }
+    void LerpPos()
+    {
+        if(resetLerp > 0)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(deathLerp?0:transform.localPosition.x, 0, 0),1f-resetLerp);
+            resetLerp -= Time.deltaTime;
+        }
+        else
+        {
+            deathLerp = false;
+        }
+    }
+    IEnumerator DisableColliderBriefly()
+    {
+        ColRef.enabled = false;
+        yield return new WaitForSeconds(0.15f);
+        ColRef.enabled = true;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+
+        Debug.Log(other.name);
+        if (other.gameObject.TryGetComponent<SplineTrack>(out SplineTrack ST)&& !grounded)
+        {
+            //landing
+            grounded=true;
+            dollykart.SplinePosition=ST.EvaluateBasedOnWorldPosition(transform.position);
+            ySpeed = 0;
+            dollykart.AutomaticDolly.Enabled = true;
+            transform.parent = dollykart.transform;
+            resetLerp = 0.3f;
+            //transform.localPosition = new Vector3(transform.localPosition.x, 0, 0);
+        }
     }
 }
--- Fjord fury/Assets/Scripts/SplineTrack.cs
+++ Fjord fury/Assets/Scripts/SplineTrack.cs
@@ -1,11 +1,13 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.Splines;
 
+
 public class SplineTrack : MonoBehaviour
 {
     public UnityEvent OnEnd;
-
+    [NonSerialized] public SplineContainer Track;
     public SplineExtrude extruder;
     public bool WidthGizmo = true;
     public float width, multiplier;
@@ -13,7 +15,9 @@ public class SplineTrack : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
+        Track = GetComponent<SplineContainer>();
+        extruder = GetComponent<SplineExtrude>();
     }
 
     // Update is called once per frame
@@ -28,5 +32,13 @@ public class SplineTrack : MonoBehaviour
             Vector3 start= extruder.Spline.EvaluatePosition(0);
             Gizmos.DrawSphere(start+transform.position, width);
         }
+    }
+    public float EvaluateBasedOnWorldPosition(Vector3 WorldPos)
+    {
+
+        SplineUtility.GetNearestPoint(Track[0], WorldPos, out Unity.Mathematics.float3 nearest, out float t);
+
+        t *= Track[0].GetLength();
+        return t;
     }
 }

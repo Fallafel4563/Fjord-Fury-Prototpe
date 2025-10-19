597954eb4554a149e19f4e0ad2d19a7ec0dad113
Fallafel4563 <70334545+Fallafel4563@users.noreply.github.com>
2025-10-19 12:43:23 +0200

    tested animation curve

---
+++ Fjord fury/Assets/Scripts/CPURacer.cs
@@ -0,0 +1,21 @@
+using System;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.Splines;
+
+public class CPURacer : MonoBehaviour
+{
+    
+    public AnimationCurve TargetPositionCurve;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
